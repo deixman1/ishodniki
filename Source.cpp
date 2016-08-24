79e13bf1d162b519f9ebfd676661d2b394a178f7
deixman1 <deix@yandex.ru>
2016-08-24 15:43:58 +0700

    rekursi9

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <ctime>
+#include <string>
+#include <conio.h>
+using namespace std;
+{
+}
+int main()
+{
+	int min, max;
+	min = 0;
+		str[i] = ' ';
+	}
+	cout << str << endl;
+								 */
+		cout << str << endl;
+			str[i] = ' ';
+		}
+	}
+	_getch();
+	return 0;
+}
